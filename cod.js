20c2b571c6607bada7c6727db11fbb1f5e421243
Domenico <14530059@itisrossi.vi.it>
2026-01-14 18:02:48 +0100

    1° versione

---
+++ cod.js
@@ -0,0 +1,46 @@
+function genera(){
+    let numeri = parseInt(document.getElementById("numero").value);
+    if (!numeri || numeri < 1 || numeri > 20){
+        document.getElementById("errore").innerHTML="Errore, inserisci un numero valido";
+        return;
+    }
+    document.getElementById("errore").innerHTML ="";
+    document.getElementById("genera").disabled=true;
+    let div = document.getElementById("div");
+    div.innerHTML="";
+    let num =[];
+    for (let i = 0; i < numeri; i++){
+        let br = document.createElement("br");
+        let testo = document.createTextNode("Scegli un numero");        
+        let input = document.createElement("input");
+        input.type = "number";
+        input.id = "numero" + i;
+        num.push("numero" + i);
+        div.appendChild(testo);
+        div.appendChild(input);
+        div.appendChild(br);
+    
+    }
+    let bottone= document.createElement("button");
+    bottone.textContent = "Genera";
+    div.appendChild(bottone);
+    let somma = 0;
+    let prodotto = 1;
+    bottone.onclick = function () {
+    calcola(num, somma, prodotto);
+    };
+}
+function calcola(num, somma, prodotto){
+    let nume= [];
+    for (let i = 0; i < num.length; i++){
+        nume.push(parseInt(document.getElementById(num[i]).value));
+    }
+    for (let i = 1; i <nume.length; i += 2){
+        somma += nume[i];
+    }
+    for (let i = 0; i <nume.length; i += 2){
+        prodotto *= nume[i];
+    }
+    document.getElementById("ris").innerHTML = "La somma dei numeri in posizione pari è :" + somma
+                                                + "<br>Il prodotto dei numeri in posizione dispari è :" + prodotto;
+}
